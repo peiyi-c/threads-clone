f82d610626671b79e1ae0948275d53f58a3722d5
peiyi-c <pennieyoyoyo@gmail.com>
2024-01-01 16:28:07 +0800

    clearup template, add favicon, title

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,38 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+import { mode } from "@chakra-ui/theme-tools";
+import { BrowserRouter } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const styles = {
+  global: (props) => ({
+    body: {
+      bg: mode("#FFFFFF", "#101010")(props),
+      color: mode("#000000", "#F3F5F7")(props),
+      fontFamily:
+        "system-ui, -apple-system, BlinkMacSystemFont, Roboto, Helvetica, Arial, sans-seri",
+      fontSize: "0.9375rem",
+      fontWeight: 400,
+      lineHeight: 1.3333,
+    },
+  }),
+};
+
+const config = {
+  initialColorMode: "system",
+  useSystemColorMode: true,
+};
+
+const theme = extendTheme({ config, styles });
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <BrowserRouter>
+      <ChakraProvider theme={theme}>
+        <App />
+      </ChakraProvider>
+    </BrowserRouter>
+  </React.StrictMode>
+);
