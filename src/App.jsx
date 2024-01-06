c3dfd9427efa248413b814ea050c776721a21627
peiyi-c <pennieyoyoyo@gmail.com>
2024-01-06 21:15:15 +0800

    install swiper.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import "./index.css";
+import "swiper/css";
+import "swiper/css/navigation";
 import Header from "./components/Header/Header";
 import { Main } from "./components/Main/Main";
 import Footer from "./components/Footer/Footer";
