45a0cb7071211019400db9bab8a75f1ab0de7b2a
peiyi-c <pennieyoyoyo@gmail.com>
2024-01-07 00:59:03 +0800

    add routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,33 @@
 import "./index.css";
 import "swiper/css";
 import "swiper/css/navigation";
-import Header from "./components/Header/Header";
-import { Main } from "./components/Main/Main";
-import Footer from "./components/Footer/Footer";
+
 import { ContentMessage } from "./contexts/contentContext";
+import { Route, Routes } from "react-router-dom";
+import PageLayout from "./layouts/PageLayout";
+import HomePage from "./pages/HomePage/HomePage";
+import SearchPage from "./pages/SearchPage/SearchPage";
+import ActivityPage from "./pages/ActivityPage/ActivityPage";
+import LoginPage from "./pages/LoginPage/LoginPage";
+import ProfilePage from "./pages/ProfilePage/ProfilePage";
 
 function App() {
   return (
     <>
       <ContentMessage>
-        <Header />
-        <Main />
-        <Footer />
+        <Routes>
+          <Route path="/" element={<PageLayout />}>
+            {/*  only for authUser */}
+            <Route index element={<HomePage />} />
+            <Route path="/search" element={<SearchPage />} />
+            <Route path="/activity" element={<ActivityPage />} />
+            {/* only for non-authUser */}
+            {/* <Route path="/login" element={<LoginPage />} />  */}
+
+            {/* authUser || non-authUser */}
+            <Route path="/:username" element={<ProfilePage />} />
+          </Route>
+        </Routes>
       </ContentMessage>
     </>
   );
