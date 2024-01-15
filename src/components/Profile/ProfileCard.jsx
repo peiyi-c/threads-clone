5fa132ee968d80140e7660620b3e0f8174ffd6e7
peiyi-c <pennieyoyoyo@gmail.com>
2024-01-15 10:34:41 +0800

    add props and skeleton

--- src/components/Profile/ProfileCard.jsx
+++ src/components/Profile/ProfileCard.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/prop-types */
 import {
   VStack,
   Flex,
@@ -6,28 +7,80 @@ import {
   Avatar,
   HStack,
   Heading,
+  Skeleton,
+  SkeletonCircle,
 } from "@chakra-ui/react";
-const ProfileCard = () => {
+
+const ProfileCard = ({ user, isLoading }) => {
+  if (isLoading) return <ProfileCardSkeleton />;
   return (
-    <VStack p={"16px 12px"} alignItems={"flex-start"}>
-      <Flex w={"full"} flexDir={"row"} justifyContent={"space-around"} gap={0}>
-        <Box>
-          <Heading fontSize={"21px"} lineHeight={"30px"}>
-            display name
-          </Heading>
-          <Text lineHeight={"21px"}>username</Text>
-        </Box>
-        <Box ml={"auto"}>
-          <Avatar size={"lg"} />
-        </Box>
-      </Flex>
-      <Text lineHeight={"21px"}>description</Text>
-      <HStack mt={"10px"}>
-        <Avatar size={"xs"} />
-        <Text as={"span"}>20 followers</Text>
-      </HStack>
-    </VStack>
+    !isLoading && (
+      <VStack p={"16px 12px"} alignItems={"flex-start"}>
+        <Flex
+          w={"full"}
+          flexDir={"row"}
+          justifyContent={"space-around"}
+          gap={0}
+        >
+          <Box>
+            <Heading fontSize={"21px"} lineHeight={"30px"}>
+              {user.displayName}
+            </Heading>
+            <Text lineHeight={"21px"}>{user.username}</Text>
+          </Box>
+          <Box ml={"auto"}>
+            <Avatar size={"lg"} />
+          </Box>
+        </Flex>
+        <Text lineHeight={"21px"}>{user.bioDescription}</Text>
+        <HStack mt={"10px"}>
+          <Avatar size={"xs"} />
+          <Text as={"span"}>
+            {user.followers.length}{" "}
+            {user.followers.length > 1 ? "followers" : "follower"}
+          </Text>
+        </HStack>
+      </VStack>
+    )
   );
 };
 
 export default ProfileCard;
+
+const ProfileCardSkeleton = () => {
+  return (
+    <>
+      <VStack p={"16px 12px"} alignItems={"flex-start"}>
+        <Flex
+          w={"full"}
+          flexDir={"row"}
+          justifyContent={"space-around"}
+          gap={0}
+        >
+          <Box>
+            <Skeleton>
+              <Heading height={"21px"} w={"full"}>
+                displayName displayName
+              </Heading>
+            </Skeleton>
+            <Skeleton>
+              <Text lineHeight={"21px"}>username</Text>
+            </Skeleton>
+          </Box>
+          <Box ml={"auto"}>
+            <SkeletonCircle size={"64px"} />
+          </Box>
+        </Flex>
+        <Skeleton>
+          <Text lineHeight={"21px"}>bioDescription</Text>
+        </Skeleton>
+        <HStack mt={"10px"}>
+          <SkeletonCircle size="10" />
+          <Skeleton>
+            <Text as={"span"}>followers</Text>
+          </Skeleton>
+        </HStack>
+      </VStack>
+    </>
+  );
+};
