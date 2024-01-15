c59beb9a27e33d97f97f6e23f7b7edfa2a53af76
peiyi-c <pennieyoyoyo@gmail.com>
2024-01-15 10:38:53 +0800

    add authUser & !authUser ui

--- src/pages/ProfilePage/ProfilePage.jsx
+++ src/pages/ProfilePage/ProfilePage.jsx
@@ -1,55 +1,75 @@
-import { Box, Button } from "@chakra-ui/react";
+import { Box, Button, Flex } from "@chakra-ui/react";
 import ProfileCard from "../../components/Profile/ProfileCard";
 import ProfileTabs from "../../components/Profile/ProfileTabs";
+import NotFoundPage from "../NotFoundPage/NotFoundPage";
+import useAuthStore from "../../store/authStore";
+import { useParams } from "react-router-dom";
+import useGetProfileByUsername from "../../hooks/useGetProfileByUsername";
 
 const ProfilePage = () => {
+  const { user } = useAuthStore();
+  const { ausername } = useParams();
+  const username = ausername.slice(1);
+  const { isLoading, userProfile } = useGetProfileByUsername(username);
+
+  const viewOwnProfileAndAuth = user?.username === username;
+  const viewOthersProfileAndAuth = user?.username !== userProfile?.username;
+  // const viewProfileAndUnauth = !user;
+  const userNotFound = !isLoading && !userProfile;
+
+  if (userNotFound && !viewOwnProfileAndAuth) return <NotFoundPage />;
   return (
     <>
       {/* Profile Card */}
-      <ProfileCard />
+      {<ProfileCard user={userProfile} isLoading={isLoading} />}
 
       {/* Button */}
       <Box my={"12px"}>
-        <Button
-          w={"full"}
-          variant={"squareOutline"}
-          _active={{
-            transform: "scale(0.95)",
-            transition: "transform 0.11s ease-in-out",
-          }}
-          fontSize={"15px"}
-        >
-          Edit Profile
-        </Button>
+        {viewOwnProfileAndAuth && (
+          <Button
+            w={"full"}
+            variant={"squareOutline"}
+            _active={{
+              transform: "scale(0.95)",
+              transition: "transform 0.11s ease-in-out",
+            }}
+            fontSize={"15px"}
+          >
+            Edit Profile
+          </Button>
+        )}
 
-        {/* <Flex justifyContent={"center"} gap={"4px"}>
-        <Button
-          w={"full"}
-          variant={"squareOutline"}
-          _active={{
-            transform: "scale(0.9)",
-            transition: "transform 0.11s ease-in-out",
-          }}
-          fontSize={"15px"}
-        >
-          Following
-        </Button>
-        <Button
-          w={"full"}
-          variant={"squareOutline"}
-          _active={{
-            transform: "scale(0.9)",
-            transition: "transform 0.11s ease-in-out",
-          }}
-          fontSize={"15px"}
-        >
-          Mention
-        </Button>
-      </Flex> */}
+        {!isLoading && viewOthersProfileAndAuth && (
+          <Flex justifyContent={"center"} gap={"4px"}>
+            <Button
+              w={"full"}
+              variant={"squareOutline"}
+              _active={{
+                transform: "scale(0.9)",
+                transition: "transform 0.11s ease-in-out",
+              }}
+              fontSize={"15px"}
+            >
+              Follow
+            </Button>
+            <Button
+              w={"full"}
+              variant={"squareOutline"}
+              _active={{
+                transform: "scale(0.9)",
+                transition: "transform 0.11s ease-in-out",
+              }}
+              fontSize={"15px"}
+            >
+              Mention
+            </Button>
+          </Flex>
+        )}
+        {/* {viewProfileAndUnauth && <></>} */}
       </Box>
 
       {/* Tabs */}
-      <ProfileTabs />
+      {!isLoading && <ProfileTabs user={userProfile} />}
     </>
   );
 };
