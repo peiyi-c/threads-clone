67fb576b9c3485a55b443a1a3837cfb0fa65cba1
peiyi-c <pennieyoyoyo@gmail.com>
2024-01-20 10:51:40 +0100

    extend media types

--- src/hooks/usePreviewImg.jsx
+++ src/hooks/usePreviewImg.jsx
@@ -4,13 +4,20 @@ import useShowToast from "./useShowToast";
 const usePreviewImg = () => {
   const [selectedFile, setSelectedFile] = useState();
   const showToast = useShowToast();
-  const maxFileSizeInBytes = 2 * 1024 * 1024; // 2MB
+  const MAX_FILE_SIZE_BYTES = 2 * 1024 * 1024; // 2MB
 
   const handleImgChange = (e) => {
     const file = e.target.files[0];
 
-    if (file && file.type.startsWith("image/")) {
-      if (file.size > maxFileSizeInBytes) {
+    if (!file) return;
+
+    const accept =
+      file.type.startsWith("image/") ||
+      file.type.startsWith("audio/") ||
+      file.type.startsWith("video/");
+
+    if (file && accept) {
+      if (file.size > MAX_FILE_SIZE_BYTES) {
         showToast("Error", "File size must be less than 2MB", "error");
         setSelectedFile(null);
         return;
