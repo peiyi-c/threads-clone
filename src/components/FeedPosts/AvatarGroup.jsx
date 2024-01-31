5f569d28ebb981ebc9ea200816278dcd64ccf3a2
peiyi-c <pennieyoyoyo@gmail.com>
2024-01-31 22:21:47 +0100

    delete reverse

--- src/components/FeedPosts/AvatarGroup.jsx
+++ src/components/FeedPosts/AvatarGroup.jsx
@@ -15,7 +15,6 @@ export const AvatarGroup1 = ({ repliedBy }) => {
 export const AvatarGroup2 = ({ repliedBy }) => {
   const borderOne = useColorModeValue("#00000026", "#f3f5f726");
   const borderTwo = useColorModeValue("transparent", "#101010");
-  repliedBy = repliedBy.reverse();
   return (
     <>
       <Avatar
@@ -41,7 +40,6 @@ export const AvatarGroup2 = ({ repliedBy }) => {
 export const AvatarGroup3 = ({ repliedBy }) => {
   const borderOne = useColorModeValue("#00000026", "#f3f5f726");
   const borderTwo = useColorModeValue("transparent", "#101010");
-  repliedBy = repliedBy.reverse();
   return (
     <>
       <Avatar
