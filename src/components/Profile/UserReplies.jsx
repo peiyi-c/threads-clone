90d0fa4dc69f2c7e3fafc8c0b9d28f1b34087cdb
peiyi-c <pennieyoyoyo@gmail.com>
2024-02-06 21:55:33 +0100

    add skeleton

--- src/components/Profile/UserReplies.jsx
+++ src/components/Profile/UserReplies.jsx
@@ -1,8 +1,19 @@
 import useGetUserReplies from "../../hooks/useGetUserReplies";
+import { FeedPostSkeleton } from "../FeedPosts/FeedPost";
 import FeedPosts from "../FeedPosts/FeedPosts";
 
 const UserReplies = () => {
   const { isLoading, replied } = useGetUserReplies();
+  if (isLoading)
+    return (
+      <>
+        {Array(Math.floor(Math.random() * 6))
+          .fill(0)
+          .map((num, index) => (
+            <FeedPostSkeleton key={index} />
+          ))}
+      </>
+    );
   if (!isLoading && replied) return <FeedPosts threads={replied} />;
 };
 
--- src/components/Profile/UserThreads.jsx
+++ src/components/Profile/UserThreads.jsx
@@ -1,8 +1,19 @@
 import useGetUserThreads from "../../hooks/useGetUserThreads";
+import { FeedPostSkeleton } from "../FeedPosts/FeedPost";
 import FeedPosts from "../FeedPosts/FeedPosts";
 
 const UserThreads = () => {
   const { isLoading, threads } = useGetUserThreads();
+  if (isLoading)
+    return (
+      <>
+        {Array(Math.floor(Math.random() * 6))
+          .fill(0)
+          .map((num, index) => (
+            <FeedPostSkeleton key={index} />
+          ))}
+      </>
+    );
   if (!isLoading) return <FeedPosts threads={threads} />;
 };
 
