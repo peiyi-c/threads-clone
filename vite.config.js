c451c77eeefa135f514892f6236b19145e331708
peiyi-c <pennieyoyoyo@gmail.com>
2024-02-11 13:01:21 +0100

    config build

--- vite.config.js
+++ vite.config.js
@@ -6,5 +6,18 @@ export default defineConfig({
   plugins: [react()],
   build: {
     chunkSizeWarningLimit: 1600,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            return id
+              .toString()
+              .split("node_modules/")[1]
+              .split("/")[0]
+              .toString();
+          }
+        },
+      },
+    },
   },
 });
