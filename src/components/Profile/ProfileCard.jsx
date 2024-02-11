fc69d6944a1910b9727b97a34a3626a67fcfac49
peiyi-c <pennieyoyoyo@gmail.com>
2024-02-11 14:56:11 +0100

    edit follower array

--- src/components/Profile/ProfileCard.jsx
+++ src/components/Profile/ProfileCard.jsx
@@ -15,7 +15,7 @@ import { Link } from "react-router-dom";
 
 const ProfileCard = ({ user, isLoading }) => {
   const subColor = useColorModeValue("#999999", "#777777");
-  const showDot = user?.followers.length > 1 && user?.bioLink;
+  const showDot = user?.followers?.length > 1 && user?.bioLink;
 
   if (isLoading) return <ProfileCardSkeleton />;
 
@@ -43,8 +43,8 @@ const ProfileCard = ({ user, isLoading }) => {
         <HStack mt={"10px"} color={subColor}>
           <Avatar size={"xs"} />
           <Text as={"span"}>
-            {user.followers.length}{" "}
-            {user.followers.length > 1 ? "Followers" : "Follower"}
+            {user.followers?.length}{" "}
+            {user.followers?.length > 1 ? "Followers" : "Follower"}
             {showDot && <Text as={"span"}> · </Text>}
             <Link target="_blank" to={user.bioLink}>
               {user.bioLink
