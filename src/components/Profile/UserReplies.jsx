3cd8a8b98d2306280dad7da9dde6703548658ff2
peiyi-c <pennieyoyoyo@gmail.com>
2024-02-11 16:33:15 +0100

    add displayReplied variable

--- src/components/Profile/UserReplies.jsx
+++ src/components/Profile/UserReplies.jsx
@@ -1,9 +1,12 @@
+import { Text, useColorModeValue } from "@chakra-ui/react";
 import useGetUserReplies from "../../hooks/useGetUserReplies";
 import { FeedPostSkeleton } from "../FeedPosts/FeedPost";
 import FeedPosts from "../FeedPosts/FeedPosts";
 
 const UserReplies = () => {
   const { isLoading, replied } = useGetUserReplies();
+  const displayReplied = !isLoading && replied;
+  const color = useColorModeValue("#000000", "#F3F5F7");
   if (isLoading)
     return (
       <>
@@ -14,7 +17,17 @@ const UserReplies = () => {
           ))}
       </>
     );
-  if (!isLoading && replied) return <FeedPosts threads={replied} />;
+  return (
+    <>
+      {displayReplied ? (
+        <FeedPosts threads={replied} />
+      ) : (
+        <Text textAlign={"center"} mt={5} color={color}>
+          No replies yet.
+        </Text>
+      )}
+    </>
+  );
 };
 
 export default UserReplies;
