bcbf6f50624ed8601ba02c3276c2cb06e5098d4a
peiyi-c <pennieyoyoyo@gmail.com>
2024-02-11 13:14:40 +0100

    edit import component

--- src/components/AuthForm/AuthForm.jsx
+++ src/components/AuthForm/AuthForm.jsx
@@ -9,8 +9,8 @@ import {
   Box,
 } from "@chakra-ui/react";
 import { Link, useLocation } from "react-router-dom";
-import LogIn from "./Login";
-import SignUp from "./SignUp";
+import LogIn from "../AuthForm/LogIn";
+import SignUp from "../AuthForm/SignUp";
 
 const AuthForm = () => {
   const { pathname } = useLocation();
