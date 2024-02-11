e53ec60191c510adf8295071db4fa934825614b1
peiyi-c <pennieyoyoyo@gmail.com>
2024-02-11 16:33:27 +0100

    add displayThreads variable

--- src/components/Profile/UserThreads.jsx
+++ src/components/Profile/UserThreads.jsx
@@ -1,9 +1,12 @@
+import { Text, useColorModeValue } from "@chakra-ui/react";
 import useGetUserThreads from "../../hooks/useGetUserThreads";
 import { FeedPostSkeleton } from "../FeedPosts/FeedPost";
 import FeedPosts from "../FeedPosts/FeedPosts";
 
 const UserThreads = () => {
   const { isLoading, threads } = useGetUserThreads();
+  const displayThreads = !isLoading && threads;
+  const color = useColorModeValue("#000000", "#F3F5F7");
   if (isLoading)
     return (
       <>
@@ -14,7 +17,17 @@ const UserThreads = () => {
           ))}
       </>
     );
-  if (!isLoading) return <FeedPosts threads={threads} />;
+  return (
+    <>
+      {displayThreads ? (
+        <FeedPosts threads={threads} />
+      ) : (
+        <Text textAlign={"center"} mt={5} color={color}>
+          No threads yet.
+        </Text>
+      )}
+    </>
+  );
 };
 
 export default UserThreads;
