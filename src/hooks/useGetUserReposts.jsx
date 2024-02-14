265425b9b9890f9ceb0c1bba8818d8d00014eede
peiyi-c <pennieyoyoyo@gmail.com>
2024-02-14 16:07:39 +0100

    reorder codes

--- src/hooks/useGetUserReposts.jsx
+++ src/hooks/useGetUserReposts.jsx
@@ -12,27 +12,29 @@ const useGetUserReposts = () => {
 
   useEffect(() => {
     const getReposts = async () => {
+      if (!userProfile) return;
       setIsLoading(true);
 
       try {
         if (userProfile.reposts.length) {
-          const reposts = [];
+          const threadRef = collection(firestore, "threads");
           // if any repost of thread exist
           const q = query(
-            collection(firestore, "threads"),
+            threadRef,
             where("id", "in", [...userProfile.reposts])
           );
-
+          const reposts = [];
           if (q) {
             const querySnapshot = await getDocs(q);
-            querySnapshot.forEach((doc) =>
-              reposts.push({ ...doc.data(), id: doc.id })
-            );
+            querySnapshot.forEach((doc) => {
+              reposts.push({ ...doc.data(), id: doc.id });
+            });
             setReposts(reposts);
           }
           // if any repost of reply exist
+          const replyRef = collection(firestore, "replies");
           const qu = query(
-            collection(firestore, "replies"),
+            replyRef,
             where("id", "in", [...userProfile.reposts])
           );
           if (qu) {
