63233cc5736c582562f235b214c99f326d9320ba
peiyi-c <pennieyoyoyo@gmail.com>
2024-02-14 14:19:58 +0100

    add logoSub, avatarBorder

--- src/hooks/useColors.jsx
+++ src/hooks/useColors.jsx
@@ -4,9 +4,20 @@ const useColors = () => {
   const whiteBlack = useColorModeValue("#FFFFFF", "#000000");
   const blackWhite = useColorModeValue("#000000", "#FFFFFF");
   const logo = useColorModeValue("#000000", "#F3F5F7");
+  const logoSub = useColorModeValue("#B8B8B8", "#4D4D4D");
   const subText = useColorModeValue("#999999", "#777777");
+  const avatarBorder1 = useColorModeValue("#00000026", "#f3f5f726");
+  const avatarBorder2 = useColorModeValue("transparent", "#101010");
 
-  return { whiteBlack, blackWhite, logo, subText };
+  return {
+    whiteBlack,
+    blackWhite,
+    logo,
+    logoSub,
+    subText,
+    avatarBorder1,
+    avatarBorder2,
+  };
 };
 
 export default useColors;
