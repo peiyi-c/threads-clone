aa4f09d97a89221d9ddc05d056b414025a1c9a25
peiyi-c <pennieyoyoyo@gmail.com>
2024-02-28 14:02:00 +0100

    add Activity component

---
+++ src/components/Activity/ActivityStyle.jsx
@@ -0,0 +1,38 @@
+export const ActivityStyle = {
+  firstPost: {
+    background: "#007aff",
+  },
+  follow: {
+    background: "#6e3def",
+  },
+  like: {
+    background: "#fe0169",
+  },
+  mention: {
+    background: "#20c584",
+  },
+  quote: {
+    background: "#fe7900",
+  },
+  polls: {
+    background: "#000000",
+  },
+  reply: {
+    background: "#24c3ff",
+  },
+  repost: {
+    background: "#c329bf",
+  },
+  request: {
+    background: "#ffb800",
+  },
+  subscribe: {
+    background: "#24c3ff",
+  },
+  warning: {
+    background: "#ff0000",
+  },
+  pinnedReply: {
+    background: "#0d31eb",
+  },
+};
