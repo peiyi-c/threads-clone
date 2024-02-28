5b21cdd71b9ecffb79509ee2d01162d3b0c7056c
peiyi-c <pennieyoyoyo@gmail.com>
2024-02-28 18:58:55 +0100

    extend timeAgo

--- src/utils/timeAgo.js
+++ src/utils/timeAgo.js
@@ -3,18 +3,24 @@ export const timeAgo = (timestamp) => {
   const secondsAgo = Math.floor((now - timestamp) / 1000);
 
   if (secondsAgo < 60) {
-    return `${secondsAgo}s ago`;
+    return `${secondsAgo}s`;
   } else if (secondsAgo < 3600) {
     const minutesAgo = Math.floor(secondsAgo / 60);
-    return `${minutesAgo}m ago`;
+    return `${minutesAgo}m`;
   } else if (secondsAgo < 86400) {
     const hoursAgo = Math.floor(secondsAgo / 3600);
-    return `${hoursAgo}h ago`;
+    return `${hoursAgo}h`;
   } else if (secondsAgo < 604800) {
     const daysAgo = Math.floor(secondsAgo / 86400);
-    return `${daysAgo}d ago`;
-  } else {
+    return `${daysAgo}d`;
+  } else if (secondsAgo < 18144000) {
     const weeksAgo = Math.floor(secondsAgo / 604800); // 7 days in seconds
-    return `${weeksAgo}w ago`;
+    return `${weeksAgo}w`;
+  } else if (secondsAgo < 6622560000) {
+    const monthsAgo = Math.floor(secondsAgo / 18144000); // 30 days in seconds
+    return `${monthsAgo}m`;
+  } else {
+    const yearsAgo = Math.floor(secondsAgo / 6622560000); // 365 days in seconds
+    return `${yearsAgo}y`;
   }
 };
